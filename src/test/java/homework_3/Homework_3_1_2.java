d7314fbf0d98196b3ccae3019d4db672ec3f65ca
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-08-23 19:22:56 +0900

    add homework 3

---
+++ src/test/java/homework_3/Homework_3_1_2.java
@@ -0,0 +1,22 @@
+package homework_3;
+
+public class Homework_3_1_2 {
+    /*
+    Необходимо создать две целочисленные переменные (a, b),
+    присвоить произвольные значения переменным на ваш выбор и вывести следующие строки:
+●	maybe a and b are even - если сумма переменных четная
+●	some variable is odd - если сумма переменных нечетная
+
+     */
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 15;
+
+        if ((a + b) % 2 == 0) {
+            System.out.println("maybe a and b are even");
+        } else {
+            System.out.println("some variable is odd");
+        }
+    }
+}
