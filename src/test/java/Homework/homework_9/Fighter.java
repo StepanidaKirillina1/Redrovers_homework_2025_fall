b5ae6fd9b522af64162059b729a19741ca40b5de
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-09-14 06:42:51 +0900

    add homework 9.1;

---
+++ src/test/java/Homework/homework_9/Fighter.java
@@ -0,0 +1,15 @@
+package Homework.homework_9;
+
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/train/java
+
+public class Fighter {
+    public String name;
+    public int health;
+    public int damagePerAttack;
+
+    public Fighter(String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+    }
+}
