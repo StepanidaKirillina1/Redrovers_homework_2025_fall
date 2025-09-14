026e99ae45ee0a951e9bc382acc80e6ac7310ff7
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-09-15 08:09:06 +0900

    add homework 10

---
+++ src/test/java/Homework/homework_10/Director.java
@@ -0,0 +1,17 @@
+package Homework.homework_10;
+
+public class Director extends Manager {
+
+    public Director(String name, double baseSalary, int numberOfSubordinates) {
+        super(name, baseSalary, numberOfSubordinates);
+    }
+
+    @Override
+    public double getSalary() {
+        if (getNumberOfSubordinates() == 0) {
+            return getBaseSalary();
+        } else {
+            return getBaseSalary() + getBaseSalary() * (getNumberOfSubordinates() / 100.0 * 9);
+        }
+    }
+}
---
+++ src/test/java/Homework/homework_10/Worker.java
@@ -0,0 +1,7 @@
+package Homework.homework_10;
+
+public class Worker extends Employee {
+    public Worker(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+}
